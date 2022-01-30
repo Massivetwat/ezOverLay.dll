372198d8ad210d9ab10d057e2a0e33e3f5e00462
Yotic <55879406+MrYotic@users.noreply.github.com>
2022-01-31 00:39:51 +0300

    Create WorthOverlay.cs

---
+++ ezOverLay/WorthOverlay.cs
@@ -0,0 +1,29 @@
+public class WorthOverlay
+{
+    private enum GetWindowType : uint
+    {
+        GW_HWNDFIRST = 0,
+        GW_HWNDLAST = 1,
+        GW_HWNDNEXT = 2,
+        GW_HWNDPREV = 3,
+        GW_OWNER = 4,
+        GW_CHILD = 5,
+        GW_ENABLEDPOPUP = 6
+    }
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr GetForegroundWindow();
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr GetWindow(IntPtr hWnd, GetWindowType uCmd);
+    private IntPtr hookWindhWnd = IntPtr.Zero;
+    public bool Discover(IntPtr window)
+    {
+        if ((int)GetWindow(GetForegroundWindow(), GetWindowType.GW_HWNDFIRST) != 0)
+            hookWindhWnd = GetForegroundWindow();
+        if (GetForegroundWindow() != window)
+            if (!(GetWindow(GetForegroundWindow(), GetWindowType.GW_HWNDFIRST) == IntPtr.Zero) && hookWindhWnd != window)
+            {
+                return false;
+            }
+        return true;
+    }
+}
